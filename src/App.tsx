4baaf3c5943b73a27e407835ab0b7e4314e60da5
Irfan Asif <irfan_2020bece060@nitsri.net>
2022-11-14 19:53:09 +0530

    build: xnft setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactXnft, { Text, View } from "react-xnft";
+
+//
+// On connection to the host environment, warm the cache.
+//
+ReactXnft.events.on("connect", () => {
+  // no-op
+});
+
+export function App() {
+  return (
+    <View>
+      <Text>Hello, World!</Text>
+    </View>
+  );
+}
